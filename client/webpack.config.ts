da7d4c52078cd1b50ecc25b38571dc363e2a89e6
lonelyua <lonelyua@gmail.com>
2025-02-08 14:42:17 +0200

    Add updates for fixing sass files

--- client/webpack.config.ts
+++ client/webpack.config.ts
@@ -1,78 +1,43 @@
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import * as path from 'path';
-import { fileURLToPath } from 'url';
-import * as webpack from 'webpack';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import _webpackDevServer from 'webpack-dev-server';
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { Configuration } from "webpack";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const config: webpack.Configuration = {
-  entry: path.resolve(__dirname, 'src/index.tsx'),
+const config: Configuration = {
+  entry: "src/index.tsx",
   output: {
-    filename: 'bundle.[hash].js',
-    path: path.resolve(__dirname, 'build'),
-    publicPath: '/',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      favicon: path.resolve(__dirname, 'public/favicon.ico'),
-      template: path.resolve(__dirname, 'public/index.html'),
-    }),
-    new MiniCssExtractPlugin(),
-    new webpack.DefinePlugin({
-      'process.env.SERVER_PORT': JSON.stringify(process.env.SERVER_PORT),
-    }),
-  ],
   resolve: {
-    modules: [path.resolve(__dirname, 'src'), path.resolve(__dirname, '../node_modules')],
-    extensions: ['.js', '.tsx', '.ts'],
+    extensions: [".ts", ".tsx", ".js"],
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
+        use: "ts-loader",
         exclude: /node_modules/,
-        resolve: {
-          extensions: ['.ts', '.tsx', '.js', '.json'],
-        },
-        use: 'ts-loader',
       },
       {
-        test: /\.(scss)$/,
-        use: [
-          { loader: MiniCssExtractPlugin.loader },
-          { loader: 'css-loader', options: { modules: true } },
-          { loader: 'sass-loader' },
-        ],
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(css)$/,
-        use: [{ loader: MiniCssExtractPlugin.loader }, { loader: 'css-loader' }],
-      },
-      {
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      },
-      {
-        test: /\.png/,
-        type: 'asset/resource',
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+  ],
   devServer: {
-    client: {
-      logging: 'none',
-    },
+    static: path.join(__dirname, "dist"),
+    hot: true,
     historyApiFallback: true,
-    port: process.env.WEBPACK_SERVER_PORT,
-  },
-  performance: {
-    hints: false,
-    maxEntrypointSize: 1024000,
-    maxAssetSize: 1024000,
   },
 };
 
