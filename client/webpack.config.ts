d29748e4872bd99a4708a0f0b92730751cc35258
lonelyua <lonelyua@gmail.com>
2025-02-08 20:38:55 +0200

    Fix building

--- client/webpack.config.ts
+++ client/webpack.config.ts
@@ -2,6 +2,7 @@ import path from "path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import { Configuration } from "webpack";
 import { fileURLToPath } from "url";
+import _webpackDevServer from "webpack-dev-server";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,7 +15,15 @@ const config: Configuration = {
     publicPath: "/",
   },
   resolve: {
+    modules: [
+      path.resolve(__dirname, "./client/src"),
+      path.resolve(__dirname, "../node_modules"),
+    ],
     extensions: [".ts", ".tsx", ".js"],
+    // alias: {
+    //   "react/jsx-dev-runtime": "react/jsx-dev-runtime.js",
+    //   "react/jsx-runtime": "react/jsx-runtime.js",
+    // },
   },
   module: {
     rules: [
@@ -22,25 +31,21 @@ const config: Configuration = {
         test: /\.(ts|tsx)$/,
         use: "ts-loader",
         exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        resolve: {
+          extensions: [".ts", ".tsx", ".js", ".json"],
+        },
       },
       {
         test: /\.scss$/,
         use: [
-          "style-loader",
-          "css-loader",
-          {
-            loader: "sass-loader",
-            // options: {
-            //   sassOptions: {
-            //     includePaths: [path.resolve(__dirname, "./client/src")],
-            //   },
-            // },
-          },
+          // { loader: MiniCssExtractPlugin.loader },
+          { loader: "css-loader", options: { modules: true } },
+          { loader: "sass-loader" },
         ],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
@@ -49,11 +54,19 @@ const config: Configuration = {
       template: "client/public/index.html",
     }),
   ],
-  // devServer: {
-  //   static: path.join(__dirname, "dist"),
-  //   hot: true,
-  //   historyApiFallback: true,
-  // },
+  devServer: {
+    client: {
+      logging: "none",
+    },
+    historyApiFallback: true,
+    // port: process.env.WEBPACK_SERVER_PORT,
+    port: 9005,
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 1024000,
+    maxAssetSize: 1024000,
+  },
 };
 
 export default config;
