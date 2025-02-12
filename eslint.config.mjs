f9568bfee9ff44a6a5f922853b44346087addd46
lonelyua <lonelyua@gmail.com>
2025-02-12 21:08:51 +0200

    Update eslint settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,13 +6,25 @@ import pluginReact from 'eslint-plugin-react';
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], settings: { react: { version: 'detect' } } },
-  // {languageOptions: { globals: globals.browser }},
+  // {
+  //   languageOptions: {
+  //     parser: tseslint.parser,
+  //     ecmaVersion: 'latest',
+  //     sourceType: 'module',
+  //     ecmaFeatures: { jsx: true },
+  //     globals: { ...globals.browser, ...globals.node, ...globals.es2021 },
+  //   },
+  // },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
     rules: {
       'react/react-in-jsx-scope': 'off',
+      indent: 'off',
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
     },
   },
   {
