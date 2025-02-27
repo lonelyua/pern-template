d38488f14ecf804e15688a8f49dcf67d6777ed13
lonelyua <lonelyua@gmail.com>
2025-02-27 19:29:24 +0200

    Add server validation

--- server/src/controllers/item.controller.ts
+++ server/src/controllers/item.controller.ts
@@ -2,28 +2,57 @@ import { Request, Response } from 'express';
 import itemService from '../services/item.service';
 
 const create = async (req: Request, res: Response) => {
-  const item = await itemService.create(req.body);
-  res.json(item);
+  try {
+    const item = await itemService.create(req.body);
+    res.json(item);
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error' });
+  }
 };
 
 const getAll = async (_req: Request, res: Response) => {
-  const items = await itemService.getAll();
-  res.json(items);
+  try {
+    const items = await itemService.getAll();
+    res.json(items);
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error' });
+  }
 };
 
 const getOne = async (req: Request, res: Response) => {
-  const item = await itemService.getOne(Number(req.params.id));
-  res.json(item);
+  try {
+    const item = await itemService.getOne(Number(req.params.id));
+    if (!item) {
+      return res.status(404).json({ message: 'Not found' });
+    }
+    res.json(item);
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error' });
+  }
 };
 
 const update = async (req: Request, res: Response) => {
-  const item = await itemService.update(Number(req.params.id), req.body);
-  res.json(item);
+  try {
+    const item = await itemService.update(Number(req.params.id), req.body);
+    if (!item) {
+      return res.status(404).json({ message: 'Not found' });
+    }
+    res.json(item);
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error' });
+  }
 };
 
 const remove = async (req: Request, res: Response) => {
-  await itemService.remove(Number(req.params.id));
-  res.sendStatus(204);
+  try {
+    const success = await itemService.remove(Number(req.params.id));
+    if (!success) {
+      return res.status(404).json({ message: 'Not found' });
+    }
+    res.sendStatus(204);
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error' });
+  }
 };
 
 export default { create, getAll, getOne, update, remove };
